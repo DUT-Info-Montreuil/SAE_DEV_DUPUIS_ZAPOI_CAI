169078de00365b51ab71e935b98b68cfd373c3cb
Your Name <you@example.com>
2026-01-07 18:02:47 +0100

    debut recap jour dans gestionnaire

---
+++ Git/gestionnaire/vue_gestionnaire.php
@@ -0,0 +1,71 @@
+<?php
+require_once('utils/vue_generique.php');
+    Class Vue_connexion extends VueGenerique{
+        public function __construct(){
+
+        }
+        
+    public function recapitulatif(){
+        echo
+            '
+            
+            '
+        ;
+    }
+
+    public function benefDuJour(){
+        echo 
+            '
+            
+            '
+        ;
+    }
+
+    public function recap_semaine(){
+        echo
+            '
+            
+            '
+        ;
+    }
+
+    public function recap_semaine(){
+        echo
+            '
+            
+            '
+        ;
+    }
+
+    public function transactionsDuJour(){
+        echo
+            '
+            
+            '
+        ;
+    }
+
+    public function Produits_vendus(){
+        echo
+            '
+            
+            '
+        ;
+    }
+    
+
+    public function menu(){
+        echo
+        '
+            <a href="index.php?module=gestionnaire&action=recap">Récapitulatif de la semaine</a>
+            
+        ';
+
+        }
+
+
+    public function affiche(){
+        return $this->getAffichage();
+    }
+    }
+?>
