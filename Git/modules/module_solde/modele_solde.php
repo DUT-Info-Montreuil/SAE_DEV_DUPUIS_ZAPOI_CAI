d6d89ba874fec67303b96f80f4242a9b79e4407f
Denis ZAPOI <deniszapoi.pro@gmail.com>
2026-01-08 14:10:48 +0100

    module solde fini

--- Git/modules/module_solde/modele_solde.php
+++ Git/modules/module_solde/modele_solde.php
@@ -7,31 +7,45 @@ class Modele_solde extends Connexion{
     public function __construct() {
 
     }
-    public function ajout_formulaire_solde(){
-        if (!isset($_POST["solde"]) || $_POST["solde"] === "") {
-            return "Champs manquants";
-        }
-        $input_solde = $_POST["solde"];
-        $idUtilisateur = $_SESSION['idUtilisateur'];
-        $sql_idCompte = "SELECT idCompte FROM compte WHERE idUtilisateur = :idU";
-        $ssql_idCompte = self::$bdd->prepare($sql_idCompte);
-        $ssql_idCompte->execute(['idU'=>$idUtilisateur]);
-        $idCompte=$ssql_idCompte->fetch(PDO::FETCH_ASSOC);
-        $sql = "INSERT into compte(idCompte,solde,idUtilisateur) values(:idC,:solde,:idUser)";
-        $ssql = self::$bdd->prepare($sql);
-        $success = $ssql -> execute([
-            'idC'=>$idCompte,
-            'solde'=>$input_solde,
-            'idUser'=>$idUtilisateur
+   public function ajout_formulaire_solde() {
 
-        ]);
-        if(!$success){
-            return "Problème avec l'ajout de solde";
-        }
-        return "Solde ajouté avec succès";
+       if (!isset($_POST["solde"]) || $_POST["solde"] === "") {
+           return "Champs manquants";
+       }
 
+       $input_solde = $_POST["solde"];
+       $idUtilisateur = $_SESSION['idUtilisateur'];
 
-    }
+       $sql_idCompte = "SELECT idCompte FROM compte WHERE idUtilisateur = :idU";
+       $ssql_idCompte = self::$bdd->prepare($sql_idCompte);
+       $ssql_idCompte->execute(['idU' => $idUtilisateur]);
+
+       $sql_solde = "SELECT solde FROM compte where idUtilisateur = :idU";
+       $ssql_solde = self::$bdd->prepare($sql_solde);
+       $ssql_solde->execute(['idU' => $idUtilisateur]);
+
+       $solde = $ssql_solde->fetchColumn();
+       $idCompte = $ssql_idCompte->fetchColumn(); // utiliser cette méthode plutôt que fetch/fetchAll , ça donne directement le contenu du tuple au lieu d'un array
+       $soldetotal= $solde+$input_solde;
+
+       $sql = "UPDATE compte
+               SET solde = :solde
+               WHERE idCompte = :idC";
+
+       $ssql = self::$bdd->prepare($sql);
+
+       $success = $ssql->execute([
+           'solde' => $soldetotal,
+           'idC'   => $idCompte
+       ]);
+
+       if (!$success) {
+           return "Problème avec la mise à jour du solde";
+       }
+
+       return "Solde mis à jour avec succès";
+   }
+
 }
 
 ?>
